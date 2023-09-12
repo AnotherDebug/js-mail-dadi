d6c8f96621f958b4348a65da8dcbda1dca2eb6e1
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-12 15:12:58 +0200

    [UPDATE]: file script

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,8 +9,10 @@ stampa un messaggio appropriato sull’esito del controllo.
 1. Chiedo con un prompt la mail dell'utente;
 2. Creo una lista che contiene le mail degli utenti registrati;
 3. Creo un ciclo for;
-4. All'interno del ciclo confronto la mail inserita con la lista attraverso un filtro;
-5. In base all'esito verrà stampato un messaggio corrispondente. 
+4. Nel ciclo dichiaro una variabile della lista con indice;
+5. All'interno del ciclo confronto la mail inserita con la lista attraverso un filtro;
+6. Creo un flag di controllo per email corrispondente;
+7. In base all'esito verrà stampato un messaggio corrispondente. 
 
 */
 
@@ -18,10 +20,24 @@ const emailListReg = [
     "antonio.faddanno@yahoo.it",
     "pippo.pluto@gmail.com",
     "pabloescobar@yahoo.it",
-    "ciccio.magro@gmail.com"
+    "ciccio.magro@gmail.com",
+    "asd"
 ];
+
+let emailFound = false;
 
 const emailRequest = prompt('Inserisci la tua E-Mail');
 
 for(let i = 0; i<emailListReg.length; i++) {
+  const emailList = emailListReg[i];
+
+  if(emailRequest === emailList) {
+    emailFound = true;
+    console.log('ciao');
+  }
+
+  if(!emailFound){
+    console.log('riprova');
+  }
+    
 };
