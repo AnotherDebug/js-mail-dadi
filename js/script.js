c3b61b844673c7684c32b1cf047bc43f69ba12ef
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-12 14:10:43 +0200

    [INIT]: file script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* 
+
+Mail
+
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+1. Chiedo con un prompt la mail dell'utente;
+2. Creo una lista che contiene le mail degli utenti registrati;
+3. Creo un ciclo for;
+4. All'interno del ciclo confronto la mail inserita con la lista attraverso un filtro;
+5. In base all'esito verrà stampato un messaggio corrispondente. 
+
+*/
+
